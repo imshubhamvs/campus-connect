ac583bcb2d39c4a39fa05c36e05c635bb94e1609
imshubhamvs <139503231+imshubhamvs@users.noreply.github.com>
2025-05-06 13:03:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,44 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://user1:shubham3011@campu
   }));
  
 
+const User = require("./models/User");
+const Post = require("./models/Post");
 
+// GET home feed
+app.get("/", async (req, res) => {
+  try {
+    const loggedInUser = req.session.user;
+    if (!loggedInUser) return res.redirect("/login");
+    const currentUser = await User.findOne({ email: req.session.user.email });
+
+    const followingEmails = currentUser.following;
+
+    // Get posts from followed users
+    const posts = await Post.find({ authorEmail: { $in: followingEmails } }).sort({
+      createdAt: -1,
+    });
+
+    const enrichedPosts = await Promise.all(
+      posts.map(async (post) => {
+        const postOwner = await User.findOne({ email: post.authorEmail });
+        return {
+          ...post._doc,
+          userPhoto: postOwner.profilePhoto,
+          username: postOwner.name,
+        };
+      })
+    );
+
+    res.render("pages/home", {
+      title: "Home Feed",
+      user: currentUser,
+      posts: enrichedPosts,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
 
 app.get('/profile', (req, res) => {
     if (!req.session.user) return res.redirect('/login');
